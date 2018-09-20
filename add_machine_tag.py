1c5c0b443aa69456573ac8c81ea77dca3e1d415a
Gerald Manipon <gmanipon@jpl.nasa.gov>
2018-09-20 18:02:28 +0000

    adding machine tags

---
+++ add_machine_tag.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+'''
+updates product grq metadata with the machine tag
+'''
+from __future__ import print_function
+import json
+import requests
+from hysds.celery import app
+
+def load_context():
+    '''loads the context file into a dict'''
+    try:
+        context_file = '_context.json'
+        with open(context_file, 'r') as fin:
+            context = json.load(fin)
+        return context
+    except:
+        raise Exception('unable to parse _context.json from work directory')
+
+def add_tag(index, uid, prod_type, tag):
+    '''updates the product with the given tag'''
+    grq_ip = app.conf['GRQ_ES_URL'].rstrip(':9200').replace('http://', 'https://')
+    grq_url = '{0}/es/{1}/{2}/{3}/_update'.format(grq_ip, index, prod_type, uid)
+    es_query = {"doc" : {"metadata": {"tags" : [tag]}}}
+    print('querying {} with {}'.format(grq_url, es_query))
+    response = requests.post(grq_url, data=json.dumps(es_query), timeout=60, verify=False)
+    response.raise_for_status()
+    print('successfully updated {} with tag {}'.format(uid, tag))
+
+def main():
+    '''loads params from context and updates the machine tag on grq'''
+    ctx = load_context()
+    index = ctx['index']
+    uid = ctx['id']
+    tag = ctx['tag']
+    prod_type = ctx['type']
+    add_tag(index, uid, prod_type, tag)
+
+if __name__ == '__main__':
+    main()
