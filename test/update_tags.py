69de13f84dd2908552603dc8005f7406c1d6b197
Marjorie Lucas <Marjorie.J.Lucas@jpl.nasa.gov>
2019-11-19 15:14:07 -0800

    added unit test

---
+++ test/update_tags.py
@@ -0,0 +1,12 @@
+def add_machine_tag():
+    print("Adding machine tag...");
+    exit 0;
+
+def main():
+    add_machine_tag();
+    #add_user_tag();
+    #remove_machine_tag();
+    #remove_user_tag();
+
+if __name__ == "__main__":
+    main()
